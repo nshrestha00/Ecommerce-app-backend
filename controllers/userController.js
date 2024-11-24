a7b3331b9ee2bda1d0e09972ceb55c1f659ed50a
Your Name <your.email@example.com>
2024-11-24 18:25:16 +0545

    Admin_login created

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,7 +25,7 @@ const loginUser= async(req,res)=>{
             res.json({success:true,token});
         }
         else{
-            res.json({success:false,message:"Invalud credentials"});
+            res.json({success:false,message:"Invalid credentials"});
         }
     } catch (error) {
         console.log(error);
@@ -77,7 +77,19 @@ const registerUser= async(req,res)=>{
 
 // Route for admin login
 const adminLogin= async(req,res)=>{
-    
+    try {
+        const {email,password}= req.body;
+
+        if(email=== process.env.ADMIN_EMAIL && password=== process.env.ADMIN_PASSWORD){
+            const token= jwt.sign(email+ password,process.env.JWT_SECRET);
+            res.json({success:true,token});
+        }else{
+            res.json({success:false,message:"Invalid credentials"});
+        }
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:error.message});
+    }
 }
 
 export {loginUser, registerUser,adminLogin}
