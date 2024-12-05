094f87238afb1437601c5c6c59a5dcdf1fa0e129
Your Name <your.email@example.com>
2024-12-05 19:19:11 +0545

    Created placeOrder controller

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,6 +1,31 @@
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+
 // placing orders using COD method
 const placeOrder= async(req,res)=>{
-    
+    try {
+        const {userId,items, amount, address}= req.body;
+
+        const orderData= {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod:"COD",
+            payment:false,
+            data:Data.now(),
+        }
+
+        const newOrder= new orderModel(orderData);
+        await newOrder.save();
+
+        await userModel.findByIdAndUpdate(userId,{cartData:{}});
+
+        res.json({success:true, message:"Order Placed"});
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message});
+    }
 }
 
 // placing orders using stripe method
